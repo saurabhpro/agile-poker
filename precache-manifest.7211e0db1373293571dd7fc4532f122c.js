0168269e6959cdb1ed869a87d3e306c1250bffd3
Saurabh Kumar <saurabhk15@outlook.com>
2020-08-15 02:13:08 +0530

    first default page

---
+++ precache-manifest.7211e0db1373293571dd7fc4532f122c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c769643e13250f383dcc7e927ca2db8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bb9b0d8137fddb455f7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "57b90559859a331857cd",
+    "url": "/static/js/2.2a116359.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2a116359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb9b0d8137fddb455f7",
+    "url": "/static/js/main.93dc173a.chunk.js"
+  },
+  {
+    "revision": "413def95ce2860603128",
+    "url": "/static/js/runtime-main.d2c58ba4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
