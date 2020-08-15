d8de156626b8aa2571278bd28a05d58f4823d38e
Saurabh Kumar <saurabhk15@outlook.com>
2020-08-15 17:38:30 +0530

    add travis and firebase config

--- src/component/constants/routes.js
+++ src/component/constants/routes.js
@@ -5,14 +5,3 @@ export const HOME = "/home";
 export const ACCOUNT = "/account";
 export const ADMIN = "/admin";
 export const PASSWORD_FORGET = "/pw-forget";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB7eDqSi97maZL5LjOSBrnbRUrqDVOKuVw",
-  authDomain: "agile-poker-sk.firebaseapp.com",
-  databaseURL: "https://agile-poker-sk.firebaseio.com",
-  projectId: "agile-poker-sk",
-  storageBucket: "agile-poker-sk.appspot.com",
-  messagingSenderId: "243041283381",
-  appId: "1:243041283381:web:59f749904068703030307c",
-  measurementId: "G-PLT1CHSW9K",
-};
