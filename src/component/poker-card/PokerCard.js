afd0b63284f4dc749b5f6e943c0e7f7edc700fe1
Saurabh Kumar <saurabhk15@outlook.com>
2020-08-15 14:13:43 +0530

    onclick mock action added

--- src/component/poker-card/PokerCard.js
+++ src/component/poker-card/PokerCard.js
@@ -7,19 +7,33 @@ import infinity from "../icon/infinity.svg";
 class PokerCard extends Component {
   state = { value: this.props.value };
 
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  getDisplayableValue = () => {
+    return this.state.value !== Infinity ? (
+      this.state.value
+    ) : (
+      <img src={infinity} alt="Infinity"></img>
+    );
+  };
+
+  onClick = (value) => {
+    alert("you clicked " + value);
+  };
+
   render() {
-    const value =
-      this.state.value !== Infinity ? (
-        this.state.value
-      ) : (
-        <img src={infinity} alt="Infinity"></img>
-      );
+    const value = this.getDisplayableValue();
+
     return (
       <div>
         <Card
           style={{ width: "10rem", height: "15em" }}
           className="m-1  pokerCard"
           id={this.state.value}
+          onClick={() => this.onClick(this.state.value)}
         >
           <Card.Body>
             <h4>{value}</h4>
