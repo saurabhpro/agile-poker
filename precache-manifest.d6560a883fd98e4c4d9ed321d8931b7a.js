71c50caa16a393aad475a6920f47659000b51a57
Saurabh Kumar <saurabhk15@outlook.com>
2020-08-16 10:55:10 +0530

    Updates

---
+++ precache-manifest.d6560a883fd98e4c4d9ed321d8931b7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83dc207669a1e077c637c6f572e4bf90",
+    "url": "/agile-poker/index.html"
+  },
+  {
+    "revision": "80f13b7183a910aef5b6",
+    "url": "/agile-poker/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7c4ae09ad201770050a7",
+    "url": "/agile-poker/static/css/main.c0376767.chunk.css"
+  },
+  {
+    "revision": "80f13b7183a910aef5b6",
+    "url": "/agile-poker/static/js/2.19df4456.chunk.js"
+  },
+  {
+    "revision": "659df5b420806b55437f4f9e57f60411",
+    "url": "/agile-poker/static/js/2.19df4456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c4ae09ad201770050a7",
+    "url": "/agile-poker/static/js/main.ee3b4911.chunk.js"
+  },
+  {
+    "revision": "351bc9314dd750f5b0c1",
+    "url": "/agile-poker/static/js/runtime-main.e77ec55f.js"
+  },
+  {
+    "revision": "cf7b1a96e4b9e67a2b31b3c59f5b3128",
+    "url": "/agile-poker/static/media/infinity.cf7b1a96.svg"
+  }
+]);
